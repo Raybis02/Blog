98d3ff3e1cff157788d676ebc558d39b3280c605
Rybis02 <rabischawarri@gmail.com>
2025-04-19 23:18:56 +0200

    added put and get

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 
-console.log('establishing connection to',mongoUrl)
+console.log('establishing connection to', mongoUrl)
 
 ;(async () => {
   console.log('smd')
   try {
+    // eslint-disable-next-line no-unused-vars
     const result = await mongoose.connect(mongoUrl)
     console.log('connected to MongoDB')
   } catch (error) {
@@ -36,6 +37,32 @@ app.get('/api/blogs', (request, response) => {
   })
 })
 
+app.get('/api/blogs', async (request, response, next) => {
+  try {
+    const entries = await Blog.find({})
+    if (entries) {
+      response.json(entries)
+    } else
+      response.status(404).send('<h1>Error 404 Page Not Found</h1><p>Database does not exist</p>')
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.get('/api/blogs/:id', async (request, response, next) => {
+  const id = request.params.id
+
+  try {
+    const entry = await Blog.findById(id)
+    if (entry)
+      response.json(entry)
+    else
+      response.status(404).send(`<h2>404 Page Not Found</h2> <p>there is no entry for a blog with id ${id}</p>`).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
 
@@ -49,10 +76,36 @@ app.delete('/api/blogs/:id', async (request, response, next) => {
   try {
     const result = await Blog.findByIdAndDelete(id)
     if (!result) {
-      return response.status(404).json( { error: 'Entry no found' } )
+      return response.status(404).json({ error: 'Entry no found' })
     }
     response.status(204).end()
 
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.put('/api/blogs/:id', async (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+
+  if ((body.title === '') || (body.author === '') || (body.url === '') || (body.likes === null)) {
+    response.status(400).json({ error: 'all fields need value' })
+  }
+
+  const entry = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  try {
+    const updatedEntry = await Blog.findByIdAndUpdate(id, entry, { new: true })
+    if (!updatedEntry) {
+      return response.status(404).json({ error: 'Entry not found' })
+    }
+    response.json(updatedEntry)
   } catch (error) {
     next(error)
   }
