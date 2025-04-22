aaaab83b34776fca35a9580f236f880e2775e24a
Rybis02 <rabischawarri@gmail.com>
2025-04-22 20:41:25 +0200

    added dummy test and totallikes test

---
+++ utils/list_helper.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  let total = 0
+  blogs.forEach(element => {
+    total += element.likes
+  })
+  return total
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
